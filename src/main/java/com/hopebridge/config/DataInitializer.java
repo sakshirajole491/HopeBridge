d6ebb037e01bac8c499bc255910c807eb46e40cb
sakshirajole491 <sakshirajole491@gmail.com>
2026-05-30 23:15:00 +0530

    Done

--- src/main/java/com/hopebridge/config/DataInitializer.java
+++ src/main/java/com/hopebridge/config/DataInitializer.java
@@ -2,6 +2,7 @@ package com.hopebridge.config;
 
 import com.hopebridge.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,35 +13,38 @@ public class DataInitializer {
 
     private final UserService userService;
 
-    //@Bean
+    @Value("${ADMIN_USERNAME}")
+    private String adminUsername;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
+
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_NAME}")
+    private String adminName;
+
+    @Value("${ADMIN_PHONE}")
+    private String adminPhone;
+
+    @Bean
     CommandLineRunner initUsers() {
         return args -> {
 
-            // Donor 1
-            if (!userService.existsByUsername("donor1")) {
-                userService.registerUser(
-                        "donor1",
-                        "donor1@hopebridge.org",
-                        "donor123",
-                        "Sakshi Rajole",
-                        "+1234567893",
-                        "ROLE_DONOR"
-                );
-            }
+            if (!userService.existsByUsername(adminUsername)) {
 
-            // Orphanage 1
-            if (!userService.existsByUsername("orphanage1")) {
                 userService.registerUser(
-                        "orphanage1",
-                        "orphanage1@hopebridge.org",
-                        "orphanage123",
-                        "Sunshine Orphanage",
-                        "+1234567891",
+                        adminUsername,
+                        adminEmail,
+                        adminPassword,
+                        adminName,
+                        adminPhone,
                         "ROLE_ADMIN"
                 );
+
+                System.out.println("Admin account created successfully.");
             }
-
-
         };
     }
 }
